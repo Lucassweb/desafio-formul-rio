ec12759ef69597f4792cca958084454870c763a1
Lucas <lucas.26.04@hotmail.com>
2023-05-08 16:17:57 -0300

    Comentario do desafio

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import {Container} from './components/Body/styles'
 import { SectionForm } from '././components/Section/style'
-
 import MinhaImagem from './assets/img/duvar.jpg'
+
+// Instruções:
+// * Você tem um formulário de login INCOMPLETO
+// * Não é permitido adicionar novos elementos HTML
+// * Não é permitido usar refs
+//
+// Tarefas:
+// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
+// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// todo - Desabilite o botão de Login equanto você está executando o login.
+// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
+// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 const App = () => {
   return (
